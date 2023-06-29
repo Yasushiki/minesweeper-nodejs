c00d5134339db409b8565673765c71782bb707c0
Yasushiki <victorg.rr2005@gmail.com>
2023-06-29 10:40:52 -0300

    win logic, lose logic, remove show all the mines in the screen with no scroll objective

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 ## todo list
 ### design
-- [ ] show all the mines in the screen size (no scroll)
 - [x] enable "custom" button in the main page
 - [?] make icons for the flag and the bomb
 ### game logic
 - [x] make numbers logic
 - [x] make all the free spaces open at once when you click then
 - [x] enable player to right-click the block so you can put a flag
-- [ ] win logic
-- [ ] lose logic
+- [x] win logic
+- [x] lose logic
 
 _made for fun_
--- public/functions.js
+++ public/functions.js
@@ -31,11 +31,11 @@ function drawBlocks(w, h) {
 			b.innerText = " ";
 			b.classList.add("x"+i);
 			b.onclick = apertar.bind(b, i, j);
-/*
+
 			if(check(blocks, i, j) == -1) {
 				b.style.backgroundColor = 'red';
 			}
-*/			
+			
 			divC.appendChild(b);
 		}
 		div.appendChild(divC);
@@ -43,7 +43,7 @@ function drawBlocks(w, h) {
 }
 
 function check(blocks, i, j) {
-	console.log(blocks[i][j]);
+	//console.log(blocks[i][j]);
 	return blocks[i][j];
 }
 
@@ -52,9 +52,11 @@ function apertar(i, j) {
 	  this.classList.add("apertado");
 	  if (check(blocks, i, j) == -1) {
 		this.style.backgroundColor = "black";
+		terminarJogo(0);
 	  } else {
 		this.style.backgroundColor = "purple";
 	  }
+
 	  if (verBomba(i, j) == 0) {
 		let buttons = getButtonsAround(i, j);
 		aoRedor(buttons);
@@ -62,10 +64,15 @@ function apertar(i, j) {
 	  if(blocks[i][j]) {
 		this.innerText = blocks[i][j];
 	  }
+	  blocks[i][j] = -2;
+
+	  if(verificaVitoria(blocks)) {
+		terminarJogo(1);
+	  }
 	}
-  }
+}
   
-  function verBomba(i, j) {
+function verBomba(i, j) {
 	let s = 0;
 	for (let x = -1; x <= 1; x++) {
 	  for (let y = -1; y <= 1; y++) {
@@ -80,18 +87,15 @@ function apertar(i, j) {
 	}
 	blocks[i][j] = s;
 	return s;
-  }
+}
   
-  function getButtonsAround(i, j) {
+function getButtonsAround(i, j) {
 	let buttons = [];
 	for (let x = -1; x <= 1; x++) {
 	  let b = document.getElementsByClassName("x" + (i + x));
 	  for (let y = -1; y <= 1; y++) {
 		try {
-		  if (
-			!b[j + y].classList.contains("apertado") &&
-			check(blocks, i + x, j + y) != -1
-		  ) {
+		  if (!b[j + y].classList.contains("apertado") && check(blocks, i + x, j + y) != -1) {
 			buttons.push(b[j + y]);
 		  }
 		} catch (error) {
@@ -100,11 +104,35 @@ function apertar(i, j) {
 	  }
 	}
 	return buttons;
-  }
+}
   
-  function aoRedor(buttons) {
+function aoRedor(buttons) {
 	for (let i = 0; i < buttons.length; i++) {
 	  buttons[i].click();
 	}
-  }
+}
   
+function verificaVitoria(array) {
+	let a = 1;
+	for(let i=0; i<array.length; i++) {
+		for(let j=0; j<array[0].length; j++) {
+			if(array[i][j] >= 0) {
+				a = 0;
+				break;
+			}
+		}
+	}
+	return a;
+}
+
+function terminarJogo(ganhou) {
+	let modal = document.getElementById("modal");
+	let p = modal.getElementsByTagName("p")[0];
+	if(ganhou) {
+		p.innerHTML = "Você ganhou!";
+	} else {
+		p.innerHTML = "Você perdeu...";
+	}
+	modal.style.display = "block";
+
+}
